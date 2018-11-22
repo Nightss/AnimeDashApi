8688e7fc1a0fa2ad114bcb6f15e512a7af6c8341
= <=>
2018-11-22 14:55:09 +0000

    create anime tables

---
+++ db/migrate/20181113101201_initial_schema.rb
@@ -0,0 +1,24 @@
+class InitialSchema < ActiveRecord::Migration[5.2]
+  def change
+    create_table :anime do |t|
+      t.text :title
+      t.text :description
+      t.string :type
+      t.date :start_date
+      t.date :end_date
+      t.integer :episode_count
+      t.binary  :picture
+      #will change to string and host images elsewhere
+    end
+
+    create_table :tags do |t|
+      t.string :name
+      t.string :description
+      t.integer :count
+    end
+
+    create_join_table :anime, :tags do |t|
+      t.index [:anime_id, :tag_id]
+    end
+  end
+end
