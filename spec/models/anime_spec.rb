0e442b54b2dce3b857a4361b0f22af6a62e6325d
= <=>
2018-11-25 12:36:29 +0000

    Change validation test to shoulda matchers syntax

--- spec/models/anime_spec.rb
+++ spec/models/anime_spec.rb
@@ -1,51 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Anime, :type => :model do
-  before(:each) do
-    @anime = Anime.new()
-    @anime.title = "God"
-    @anime.description = "Story of Jordan"
-    @anime.type = "TV"
-    @anime.start_date = Date.today
-    @anime.end_date = Date.today.next_quarter
+  # let(:anime) { Anime.new(
+  #   title: "God",
+  #   description: "Story of Jordan",
+  #   type: "TV",
+  #   start_date: Date.today,
+  #   end_date: Date.today.next_quarter
+  #   ) }
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:description)}
+  it {should validate_presence_of(:type)}
+  it {should validate_presence_of(:start_date)}
 
-    @tag = Tag.new()
-    @tag.name = "Tragedy"
-  end
+  # before(:each) do
+  #   @anime = Anime.new()
+  #   @anime.title = "God"
+  #   @anime.description = "Story of Jordan"
+  #   @anime.type = "TV"
+  #   @anime.start_date = Date.today
+  #   @anime.end_date = Date.today.next_quarter
+  #
+  #   @tag = Tag.new()
+  #   @tag.name = "Tragedy"
+  # end
 
-  describe "Validations" do
-    it "is valid with valid attributes" do
-      expect(@anime).to be_valid
-    end
-
-    it "is not valid without a title" do
-      @anime.title = nil
-      expect(@anime).to_not be_valid
-    end
-
-    it "is not valid without a description" do
-      @anime.description = nil
-      expect(@anime).to_not be_valid
-    end
-
-    it "is not valid without a type" do
-      @anime.type = nil
-      expect(@anime).to_not be_valid
-    end
-
-    it "is not valid without a start_date" do
-      @anime.start_date = nil
-      expect(@anime).to_not be_valid
-    end
-
-    it "is not valid without a end_date" do
-      @anime.end_date = nil
-      expect(@anime).to_not be_valid
-    end
-  end
-
-  describe "Associations" do
-    @anime.tags << @tag
-    it { should have_and_belong_to_many(:tags) }
-  end
+  # describe "Associations" do
+  #   @anime.tags << @tag
+  #   it { should have_and_belong_to_many(:tags) }
+  # end
 end
